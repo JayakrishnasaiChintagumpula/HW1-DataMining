a6fb6e2e07cd05b73c92b996f7c31d157a551e9e
JayakrishnasaiChintagumpula <146905031+JayakrishnasaiChintagumpula@users.noreply.github.com>
2024-01-30 06:08:28 +0530

    Update question10.py

--- question10.py
+++ question10.py
@@ -2,31 +2,51 @@ def question10_1():
     """
     answer: python bool (True or False)
     """
+    answer: bool =  False
+    # Justification: Count data can still be affected by noise, including measurement error and variability, despite its discrete nature.
     return answer
 
 def question10_2():
+    answer: bool =  True
+    # Justification: The correlation coefficient is normalized to a range of -1 to 1, reflecting the strength and direction of the linear relationship between two sets of real values.
     return answer
 
 def question10_3():
+    answer: bool =  False
+    # Justification: The choice between sampling and aggregating for reducing the size of a daily time series depends on the analysis goals and the trade-off between preserving details (aggregating) and simplifying the dataset (sampling).
     return answer
 
 def question10_4():
+    answer: bool =  False
+    #Justification: Noise represents random variations in data, while outliers are distinct data points that deviate significantly, making them separate concepts in data analysis.
     return answer
 
 def question10_5():
+    answer: bool =  False
+    # Justifiaction: An object being an outlier does not necessarily mean it is noise; outliers are distinct data points deviating significantly, while noise represents random variations in the data.
     return answer
 
 def question10_6():
+    answer: bool =  False
+    # Justification: A binary attribute with values 0 or 1 is symmetric because the two values are equally meaningful and do not imply any inherent order or preference
     return answer
 
 def question10_7():
+    answer: bool =  False
+    # Justification: A cosine measure of 1 indicates that vectors have the same direction, not necessarily that they are identical; magnitude differences can still exist.
     return answer
 
 def question10_8():
+    answer: bool =  False
+    # Justification: Discrete variables can be ratio, as long as they have a true zero point, allowing meaningful ratios (e.g., counts of items, where zero represents the absence)
     return answer
 
 def question10_9():
+    answer: bool =  False
+    # Justification: Quantitative variables can be either continuous or discrete. Continuous variables can take any value within a range (e.g., height), while discrete variables can only take distinct, separate values (e.g., number of children)
     return answer
 
 def question10_10():
+    answer: bool =  False
+    #Justification: Converting ordinal variables to asymmetric binary variables may lead to a loss of information as it ignores the ordinal nature and reduces the data to binary categories without preserving the order or magnitude between the original values
     return answer
